19b9a4771ec65482b9fbc4b5d28efb7b2c7eff8c
ArunBee <aks.bsmh@gmail.com>
2023-03-06 18:08:03 +0530

    main

---
+++ noderedx/src/User.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const User=(props)=>{
+    const {data}=props
+return(
+    <div>
+        <h2>New User Created</h2>
+        <h3>{data.name}</h3>
+        <h3>{data.age}</h3>
+    </div>
+)
+}
+export default User;
